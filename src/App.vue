099c45859379551ad332031ce934fb1decdbd5cd
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-10-15 14:32:40 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<template>
+  <main>
+    <NavBar />
+  </main>
+</template>
+
+<style scoped>
+main {
+    background-color: var(--backgroud-color);
+    min-height: 100vh;
+    min-width: 80vw;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Logo from "/src/assets/logo-vec.svg";
+</script>
+
+<template>
+  <nav class="px-12 py-8">
+    <img :src="Logo" alt="juke sites logo" class="w-20" />
+  </nav>
+</template>
